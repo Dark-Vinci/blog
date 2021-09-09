b3edc6561852e190ea7f860c2138b90aa6693438
Dark-Vinci <ademolaolutomiwa4real@gmail.com>
2021-09-09 02:12:33 -0700

    start script added

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/bloggie',{
+    const db = config.get('db');
+
+    mongoose.connect(db, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
     })
-        .then(() => console.log('connected to db...'))
+        .then(() => console.log('connected to db...', db))
         .catch((ex) => console.log('couldnt connect to the db..'));
 }
